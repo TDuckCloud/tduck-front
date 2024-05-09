3053d57f0d4efe3e76ad28acc46542fee4ed226f
smalljop <250543222@qq.com>
2024-05-09 11:01:56 +0800

    fix: TduckFormMixin.js

--- src/views/form/TduckFormMixin.js
+++ src/views/form/TduckFormMixin.js
@@ -8,17 +8,19 @@ export default {
       formKey: null
     }
   },
-  created() {
-    // 写入值到localStorage 给组件使用
-    if (store.state.user.token) {
-      localStorage.setItem(TduckForm.constant.ACCESS_TOKEN, store.state.user.token)
-    }
+  beforeCreate() {
     localStorage.setItem('FORM-SUPPORT', process.env['VUE_APP_TITLE'])
     localStorage.setItem(TduckForm.constant.ACCESS_TOKEN_NAME, 'token')
     if (process.env['VUE_APP_API_ROOT']) {
       localStorage.setItem(TduckForm.constant.BASE_URL, process.env['VUE_APP_API_ROOT'])
     } else {
       localStorage.setItem(TduckForm.constant.BASE_URL, getCurrentDomain())
+    }
+  },
+  created() {
+    // 写入值到localStorage 给组件使用
+    if (store.state.user.token) {
+      localStorage.setItem(TduckForm.constant.ACCESS_TOKEN, store.state.user.token)
     }
   }
 }
